62eaab1ab05669bef4118a69fbfa44fff1335265
roomasa <75815560+roomasa@users.noreply.github.com>
2021-03-11 16:05:12 -0600

    creating a table

---
+++ static/js/app.js
@@ -0,0 +1,26 @@
+// import the data from data.js
+const tableData = data;
+// Reference the HTML table using d3
+//tbody.htm clears all existing data in the table.
+var tbody = d3.select("tbody");
+
+//to create a table from the data array
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
